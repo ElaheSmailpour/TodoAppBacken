a383f633ad0f4f3001f3a55925a1a36c8fd07f3f
Elahesmailpour <eli_es87@yahoo.com>
2021-02-23 21:11:52 +0100

    new

--- controller/usertodoapp-controller.js
+++ controller/usertodoapp-controller.js
@@ -1,7 +1,8 @@
 
 const { validationResult } = require('express-validator')
 const TodoApp = require("../models/usertodoapp")
-
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 //get
 exports.Datenholen = (req, res, next) => {
 	TodoApp.find().then((ergebnis) => {
@@ -13,26 +14,33 @@ exports.Datenholen = (req, res, next) => {
 }
 
 //post:
-exports.erstelleaufgabe = (req, res, next) => {
-	const nutzer = req.body;
-	const errors = validationResult(req)
+exports.erstelleaufgabe = async (req, res, next) => {
+	try {
+		const nutzer = req.body;
+	
+		const errors = validationResult(req)
+	
+		if (!errors.isEmpty()) {
+		
+			return res.status(422).json({
+				fehlerBeiValidierung: errors.array()
+			})
+		}
+		
+		let schonVorhandenUser = await TodoApp.find({ email: nutzer.email })
+		if (schonVorhandenUser.length >= 1) {
+			return res.status(409).send('Es gib schon einen Nutzer mit dieser Email')
+		}
 
-	if (!errors.isEmpty()) {
-console.log(errors.array())
-		return res.status(422).json({
-			fehlerBeiValidierung: errors.array()
-		})
+		let passwortGehashed = await bcrypt.hash(nutzer.passwort, 10)
+		let erstelleNutzer = await TodoApp.create({ ...nutzer, passwort: passwortGehashed })
+		res.status(201).send(erstelleNutzer);
+
+	} catch (fehler) {
+		res.status(500).send('Da lief was schief!')
 	}
-	TodoApp.create(nutzer).then(
-		(ergebnis) => {
-			res.status(201).send(ergebnis);
-		}
-	).catch(
-		(fehler) => {
-			res.status(500).send(fehler);
-		}
-	)
 }
+
 //get mit id
 exports.Datenholenmitid = (req, res, next) => {
 	const { _id } = req.params;
@@ -53,6 +61,9 @@ exports.Datenholenmitid = (req, res, next) => {
 exports.update = (req, res, next) => {
 	const { _id } = req.params;
 	const nutzerDaten = req.body
+    if (_id !== req.tokenNutzer.userId) {
+        return res.status(401).send('Sie sind nicht nutzer und Sie dürfen nicht hier etwas ändern!')
+    }
 	const errors = validationResult(req)
 	if (!errors.isEmpty()) {
 		return res.status(422).json({
@@ -84,4 +95,35 @@ exports.Aufgabelöschen=(req,res,next)=>{
 		)
 }
 
+
+exports.nutzerEinloggen = async (req, res, next) => {
+	let nutzer = req.body
+	try {
+		
+		let userVonDatenbank = await TodoApp.findOne({ email: nutzer.email })
+		console.log(userVonDatenbank);
+	
+		if (userVonDatenbank === null) {
+			return res.status(401).send('Du konntest nicht eingeloggt werden')
+		}
+	
+		let vergleichVonPasswort = await bcrypt.compare(nutzer.passwort, userVonDatenbank.passwort)
+		
+		if (vergleichVonPasswort) {
+
+			let token = jwt.sign({
+				email: userVonDatenbank.email,
+				userId: userVonDatenbank._id,
+			}, process.env.JWT || 'ein Geheimnis', {expiresIn: '3h'})
+			res.status(200).json({
+				nachricht: 'Du bist eingeloggt',
+				token: token
+			})
+		} else {
+			res.status(401).send('Du konntest nicht eingeloggt werden')
+		}
+	} catch (error) {
+		res.status(401).send('Du konntest nicht eingeloggt werden')
+	}
+}
 
--- routes/usertodoapp.js
+++ routes/usertodoapp.js
@@ -2,26 +2,29 @@ const express = require('express');
 const router = express.Router();
 const { check } = require("express-validator")
 
+const auth = require("../middleware/auth")
+
 
 const {
-  erstelleaufgabe, Datenholen,Datenholenmitid,update,Aufgabelöschen
+  erstelleaufgabe, Datenholen, Datenholenmitid, update, Aufgabelöschen, nutzerEinloggen
 } = require('../controller/usertodoapp-controller');
 
 
 const validAufgabeUpdate = [
   check("email", "geben Sie bitte Ihre email!").trim().isEmail(),
 
-  check ("passwort","geben Sie bitte Ihre passwort!").not().isEmpty().isStrongPassword()
+  check("passwort", "geben Sie bitte Ihre passwort!").not().isEmpty().isStrongPassword()
 ]
 router
   .route('/')
   .get(Datenholen)
   .post(validAufgabeUpdate, erstelleaufgabe)
-  
-  router.route("/:_id")
+
+router.route("/:_id")
   .get(Datenholenmitid)
-  .put(validAufgabeUpdate,update)
-.delete(Aufgabelöschen)
+  .put(auth, validAufgabeUpdate, update)
+  .delete(auth, Aufgabelöschen)
+router.route('/login').post(nutzerEinloggen)
 
 
 
